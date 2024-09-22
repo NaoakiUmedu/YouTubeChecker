b1c4a17dd01cd38034ba79c844fe8534d03de157
Naoaki Umezu <n.umezu1014@gmail.com>
2024-09-22 21:24:06 +0900

    パスをミスってたのを治した

--- ExecuteApp.bat
+++ ExecuteApp.bat
@@ -1,6 +1,6 @@
-REM ★パスはtestapp.pyが置かれているディレクトリに書き換えること
-SCRIPT_HOME=C:\Hoge\Piyo\
-cd %SCRIPT_HOME%\testapp
+REM パスはtestapp.pyが置かれているディレクトリに書き換える
+SCRIPT_HOME=C:\Hoge\Piyo
+cd %SCRIPT_HOME%
 call .\venv\Scripts\activate.bat
 python testapp.py
 pause
